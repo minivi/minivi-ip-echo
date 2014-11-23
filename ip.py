81078e5fa5c27b91f7d460cc87f9e84f39781985
David Santoso <david.santoso@minivi.com>
2014-11-24 00:05:49 +0700

    add IP version test

--- ip.py
+++ ip.py
@@ -1,12 +1,24 @@
 import webapp2
+import ipaddr
 
-class MainPage(webapp2.RequestHandler):
-
+class Default(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(self.request.remote_addr)
 
+class CheckIPVersion(webapp2.RequestHandler):
+    def get(self, ip_version):
+        self.response.headers['Content-Type'] = 'text/plain'
+        ip=ipaddr.IPAddress(self.request.remote_addr, version=ip_version)
 
+        if ip_version == '4':
+            self.response.write(isinstance(ip,ipaddr.IPv4Address))
+        elif ip_version == '6':
+            self.response.write(isinstance(ip,ipaddr.IPv6Address))
+
+        
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', Default),
+    (r'/isipv(4|6)', CheckIPVersion),
 ])
+
