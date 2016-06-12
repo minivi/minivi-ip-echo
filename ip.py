748c8f5efd79a80bb00b4e80944f86b8ac3b7ed4
David Santoso <david.santoso@minivi.com>
2016-06-12 21:25:07 +0700

    modify challenge-response for letsencrypt

--- ip.py
+++ ip.py
@@ -24,7 +24,8 @@ class LetsEncryptHandler(webapp2.RequestHandler):
     def get(self, challenge):
         self.response.headers['Content-Type'] = 'text/plain'
         responses = {
-                    'rldLVXbWmljTa0Vnf-jcgTX0cYBN7javEvFk008Awpk': 'rldLVXbWmljTa0Vnf-jcgTX0cYBN7javEvFk008Awpk.-6tKHL8U8EMsqcdR-VVBfXLMb2iqPOysH0Y2xWlkJ7E',
+            'rldLVXbWmljTa0Vnf-jcgTX0cYBN7javEvFk008Awpk': 'rldLVXbWmljTa0Vnf-jcgTX0cYBN7javEvFk008Awpk.-6tKHL8U8EMsqcdR-VVBfXLMb2iqPOysH0Y2xWlkJ7E',
+            'u2sosNZKagrUOFrweofVuEBu--njhnrEjBy81kvYqnI': 'u2sosNZKagrUOFrweofVuEBu--njhnrEjBy81kvYqnI.aATglod0Ki8gH8iAIPXUShPu6jEMA_z3QPVXaMn7RjY',
                 }
         self.response.write(responses.get(challenge, ''))
 
